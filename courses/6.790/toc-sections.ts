2bd46b7f566b72bb538c955f3191b9ba9fdcdb6b
Jason Mao <jmao142857@gmail.com>
2026-09-09 16:33:26 -0400

    6.790: minor fixes

--- courses/6.790/toc-sections.ts
+++ courses/6.790/toc-sections.ts
@@ -12,5 +12,6 @@ export const TOC_PARTS: TocPart[] = [
   { title: "Prerequisites", from: 1, to: 3 },
   { title: "Regression", from: 4, to: 8 },
   { title: "Optimization and Learnability", from: 9, to: 11 },
-  { title: "Neural Networks", from: 12, to: 15 },
+  { title: "Neural Networks", from: 12, to: 14 },
+  { title: "Learning and Models", from: 15, to: 16 },
 ];
